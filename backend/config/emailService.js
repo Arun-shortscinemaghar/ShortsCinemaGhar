77bb9df7d25970db5e6454ff19be9e93d9e4e30a
komal-kumawat <kumawatkomal17122004@gmail.com>
2026-03-20 18:30:04 +0530

    commit it

--- backend/config/emailService.js
+++ backend/config/emailService.js
@@ -1,9 +1,11 @@
 const nodemailer = require('nodemailer');
+const dns = require('dns');
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
